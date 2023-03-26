63c9a696c7afd500808d205960a2d7ca998f2d6c
ibeenoch <fredenoch1@gmail.com>
2023-03-26 08:39:51 +0100

    email sub backend

--- index.js
+++ index.js
@@ -3,6 +3,9 @@ import dotenv from 'dotenv';
 import cors from 'cors';
 import connectdb from './config/db.js';
 import emailRoute from './route/email.js';
+import { fileURLToPath } from 'url';
+import path from 'path'
+
 
 const app = express();
 
@@ -12,7 +15,27 @@ connectdb();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }))
-app.use(cors())
+app.use(cors());
+
+// app.use(function(req, res, next) {
+//     res.header("Access-Control-Allow-Origin", "*");
+//     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+//     res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+//     next();
+//   });
+
+// get dirname
+const dirname = fileURLToPath(new URL('.', import.meta.url));
+//serving css files
+app.use(express.static(dirname));
+
+app.get('/', function(req, res) {
+    res.sendFile(path.join(dirname, '../maalu-landing-page/index.html'))
+})
+
+app.get('/home', (req, res) => {
+    console.log('homepage')
+})
 
 app.use('/', emailRoute)
 
